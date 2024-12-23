c85593ad5842ff5cfe2f7e5f05c13236b79936e1
Anirban Prince <anirbanprince@yahoo.com>
2024-12-23 17:59:00 +0600

    Test Point Added

--- app/src/main/java/com/proseobd/testpoint/ISPFragment.java
+++ app/src/main/java/com/proseobd/testpoint/ISPFragment.java
@@ -7,9 +7,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class ISPFragment extends Fragment {
+
+    TextView itemTv;
 
 
 
@@ -17,6 +20,8 @@ public class ISPFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View fragmentView = inflater.inflate(R.layout.fragment_isp, container, false);
+
+        itemTv = fragmentView.findViewById(R.id.itemTv);
 
 
 
